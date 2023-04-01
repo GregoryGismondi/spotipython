867787ebf823f573f829af8584e8f8765cebf5aa
Amy Li <110435081+ayyli208@users.noreply.github.com>
2023-04-01 19:26:28 -0400

    Update spotipy_main_classes.py

--- spotipy_main_classes.py
+++ spotipy_main_classes.py
@@ -14,6 +14,31 @@ app_client_secret = "1aa0b1b3d6a94f00a1125c24394a886e"
 client_credentials_manager = SpotifyClientCredentials(client_id=app_client_id, client_secret=app_client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+class User:
+    """
+    Information about the user's preferences.
+    Contains the user's favorite song, the corresponding artist, the user's desired diversity level,
+    and the user's favorite attribute about their favorite song, if any.
+
+    Instance Attributes:
+        - song_name: The name of the user's favorite song
+        - artist_name: The name of the corresponding artist to song_name
+        - diversity_level: an integer greater than or equal to zero. This integer corresponds to the depth
+        of the artist tree, with a larger level of 'diversity' representing a deeper depth into the tree of artists.
+        If the user chooses not to input a number, 'diversity' defaults to 0.
+    """
+    song_name: str
+    artist_name: str
+    diversity_level: Optional[int] = 0
+
+    def __init__(self, song_name: str, artist_name: str, diversity_level: Optional[int] = 0) -> None:
+        """Initialize a new User object. Contains the user's preferences.
+        """
+        self.song_name = song_name
+        self.artist_name = artist_name
+        self.diversity_level = diversity_level
+
+
 class SongInfo:
     """
     Information about a song.
