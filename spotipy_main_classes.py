65bbf2b4c5604b8eee151f4cd6606d9a0ad34779
Amy Li <110435081+ayyli208@users.noreply.github.com>
2023-04-01 22:16:06 -0400

    Update spotipy_main_classes.py

--- spotipy_main_classes.py
+++ spotipy_main_classes.py
@@ -28,18 +28,21 @@ class User:
         - diversity_level: an integer greater than or equal to zero. This integer corresponds to the depth
         of the artist tree, with a larger level of 'diversity' representing a deeper depth into the tree of artists.
         If the user chooses not to input a number, 'diversity' defaults to 0.
+        - fav_attribute: The user's favorite attribute about their chosen song, if any.
     """
     song_name: str
     artist_name: str
     diversity_level: Optional[int] = 0
+    fav_attribute: Optional[str]
 
-    def __init__(self, song_name: str, artist_name: str, diversity_level: Optional[int] = 0) -> None:
+    def __init__(self, song_name: str, artist_name: str, fav_attribute: Optional[str],
+                 diversity_level: Optional[int] = 0) -> None:
         """Initialize a new User object. Contains the user's preferences.
         """
         self.song_name = song_name
         self.artist_name = artist_name
         self.diversity_level = diversity_level
-
+        self.fav_attribute = fav_attribute
 
 class SongInfo:
     """
