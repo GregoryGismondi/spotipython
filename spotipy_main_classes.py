cbddc8ca6e0b864e6463dd87898f4fe3c14fa349
brittany <121523439+brittanylansang@users.noreply.github.com>
2023-04-01 22:01:13 -0400

    Add files via upload

--- spotipy_main_classes.py
+++ spotipy_main_classes.py
@@ -1,20 +1,20 @@
 """ Contains the SongInfo, ArtistNode, and ArtistTree Class"""
 from __future__ import annotations
 from typing import Optional
+
 # Comment out this line when you aren't using check_contracts
 from python_ta.contracts import check_contracts
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
-from test import artist_5_top_songs
-from spotipy_create_obj import create_song_info
+from test import artist_five_tracks, artist_id
 
 app_client_id = "10ad55033d8d48dc9b90c9aa1e6d074c"
 app_client_secret = "1aa0b1b3d6a94f00a1125c24394a886e"
 
 client_credentials_manager = SpotifyClientCredentials(client_id=app_client_id, client_secret=app_client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 class User:
     """
@@ -76,18 +76,21 @@ class SongInfo:
     energy: float
     acousticness: float
 
-    def __init__(self, song_name: str, artist_name: str, danceability: float,
-                 valence: float, tempo: float, instrumentalness: float, energy: float, acousticness: float) -> None:
+    def __init__(self, song_name: str, artist_name: str) -> None:
         """Initialize a new SongNode.
         """
-        self.song_name = song_name
-        self.artist_name = artist_name
-        self.danceability = danceability
-        self.valence = valence
-        self.tempo = tempo
-        self.instrumentalness = instrumentalness
-        self.energy = energy
-        self.acousticness = acousticness
+        tracks = sp.search(q=song_name, type='track')['tracks']
+        items = tracks['items']
+        for item in items:
+            if item['name'] == song_name and item['artists'][0]['name'] == artist_name:
+                features = sp.audio_features(item['id'])[0]
+                self.song_name = song_name
+                self.artist_name = artist_name
+                self.danceability = features['danceability']
+                self.valence = features['valence']
+                self.instrumentalness = features['instrumentalness']
+                self.energy = features['energy']
+                self.acousticness = features['acousticness']
 
 
 class ArtistNode:
@@ -104,13 +107,13 @@ class ArtistNode:
     artist_id: str
     top_tracks: list[SongInfo]
 
-    def __init__(self, artist_name, artist_id) -> None:
+    def __init__(self, artist_name) -> None:
         """Initialize a new ArtistNode.
         """
         self.artist_name = artist_name
-        self.artist_id = artist_id
-        top_tracks = artist_5_top_songs(artist_name)
-        song_info_tracks = [create_song_info(x, top_tracks[x]) for x in top_tracks]
+        self.artist_id = artist_id(artist_name)
+        top_tracks = artist_five_tracks(artist_name)
+        song_info_tracks = [SongInfo(song_name, top_tracks[song_name]) for song_name in top_tracks]
         self.top_tracks = song_info_tracks
 
 
@@ -118,119 +121,44 @@ class ArtistTree:
     """A tree representing the user's favorite song's artist as the root, and similar artists as the subtrees.
 
         Each node in the tree is an ArtistNode
+
+        Instance Attributes:
+            - artist: the current node of the tree
+            - depth: the current depth of the tree
         """
+    artist: ArtistNode
+    depth: int
 
     # Private Instance Attributes:
-    #  - artist: the ArtistNode root of the tree
     #  - _subtrees:
     #      the subtrees of this tree, which represent similar artists to its parent node.
     #      _subtrees will be None if we reach the user's given diversity level
-    _artist: Optional[ArtistNode]
-    _subtrees: list[ArtistTree]
+    _subtrees: Optional[list[ArtistNode]]
 
-    def __init__(self, artist: Optional[ArtistNode], subtrees: list[ArtistTree]) -> None:
+    def __init__(self, artist: ArtistNode, depth: int, _subtrees: Optional[list[ArtistNode]]) -> None:
         """Initialize a new artist tree.
 
         Note that this initializer uses optional arguments.
 
         """
-        self._artist = artist
-        self._subtrees = subtrees
+        self.artist = artist
+        self.depth = depth
+        self._subtrees = _subtrees
 
-    def __contains__(self, artist: ArtistNode) -> bool:
-        """Return whether this tree contains given the given ArtistNode
-        """
-        if self.is_empty():
-            return False
-        elif not self._subtrees:
-            return self._artist.artist_id == artist.artist_id
-        else:
-            if self._artist.artist_id == artist.artist_id:
-                return True
-            for subtree in self._subtrees:
-                if subtree.__contains__(artist):
-                    return True
-            return False
-
-    def is_empty(self) -> bool:
-        """Return whether this tree is empty.
-        """
-        return self._artist is None
-
-    def get_subtree(self, artist: ArtistNode) -> ArtistTree | None:
+    def get_subtrees(self) -> list[ArtistTree]:
         """Return the subtrees of this artist tree."""
-        if artist not in self:
-            return None
+        return list(tree for tree in self._subtrees)
 
-        if self._artist.artist_id == artist:
-            return self
-
-        for subtree in self._subtrees:
-            if artist in subtree:
-                return subtree.get_subtree(artist)
-
-    def populate_subtrees(self, artist: ArtistNode) -> None:
-        """Adds related artists as subtrees to the tree whose root is the given artist
-
-        Preconditions:
-        - artist in self
-        """
-        related_artists_dict = artist_five_related(artist)
-        related_artists_list = []
-
-        for related_artist in related_artists_dict:
-            related_artist_node = ArtistNode(related_artist, related_artists_dict[related_artist])
-            if related_artist_node not in self:
-                related_artists_list.append(related_artist_node)
-
-        if not related_artists_list:
-            return
-
-        self._insert_subtrees(related_artists_list, artist)
-
-    def _insert_subtrees(self, artists: list[ArtistNode], parent_node: ArtistNode) -> None:
+    def insert_subtrees(self, artists: list[ArtistNode], parent_node: ArtistNode) -> None:
         """Inserts the subtree(s) as subtrees of the parent node.
         Note: artists are the related artists to the parent node"""
-        if self.is_empty() or artists == []:
-            return None
-
-        elif self._artist == parent_node:
-            for artist in artists:
-                artist_tree = ArtistTree(artist, [])
-                self._subtrees.append(artist_tree)
-
-        else:
-            for subtree in self._subtrees:
-                subtree._insert_subtrees(artists, parent_node)
-
-
-def artist_five_related(artist: ArtistNode) -> dict:
-    """Get the top 5 most related artists to the input artist. Return a dictionary where the key is the artist's
-    name and the value is the artist's id.
-    """
-    related_artist = sp.artist_related_artists(artist.artist_id)
-
-    related_users = {}
-    for external_urls in related_artist['artists']:
-        if len(related_users) == 5:
-            return related_users
-        else:
-            related_users[external_urls['name']] = external_urls['id']
-
-    return related_users
+        self._subtrees = artists
 
 
 if __name__ == '__main__':
     import doctest
 
     doctest.testmod(verbose=True)
-
-    selena = ArtistNode('Selena Gomez', '0C8ZW7ezQVs4URX5aX7Kqx')
-    taylor = ArtistNode('Taylor Swift', '06HL4z0CvFAxyc27GXpf02')
-    nicki = ArtistNode('Nicki Minaj', '0hCNtLu0JehylgoiP8L4Gh')
-    sza = ArtistNode('SZA', '7tYKF4w9nC0nq9CsPZTHyP')
-
-    tree = ArtistTree(taylor, [ArtistTree(selena, [ArtistTree(nicki, [])]), ArtistTree(sza, [])])
 
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
